ffb2d3975404da0182dcad3c6eece2e3be19c311
boglarkaszabo007 <hitman05061@gmail.com>
2026-04-22 19:03:45 +0200

    database.php átalakítása localhostos nézet miatt.

--- Database/database.php
+++ Database/database.php
@@ -1,12 +1,26 @@
 <?php
-$host = "localhost";
-$user = "feltalalok";
-$pass = "feltalalok2026.";
-$db   = "feltalalok";
 
-$conn = new mysqli($host, $user, $pass, $db);
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
 
-if ($conn->connect_error) {
-    die("Kapcsolódási hiba: " . $conn->connect_error);
+    // XAMPP
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "feltalalok";
+
+} else {
+
+    // Nethely
+    $host = "localhost";
+    $user = "feltalalok";
+    $pass = "feltalalok2026.";
+    $db   = "feltalalok";
 }
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+if($conn->connect_error){
+    die($conn->connect_error);
+}
+
 ?>
